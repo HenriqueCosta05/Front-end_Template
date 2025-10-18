cf4e4ea4f4f5e95f873cb604592183b03452493c
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2025-10-18 16:03:07 -0300

    initialize project structure with essential files and configurations

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Front-End Template
 
 ## Introdução
+
 Esse projeto tem o objetivo de reunir lógicas de inicialização de projetos front-end em um único repositório! Tais configurações incluem:
+
 - [x] Configurações de `linting`, usando EsLint e Prettier.
 - [x] Hooks de `pré-commit`, que executam scripts antes de realizar um commit ao repositório, utilizando Husky e GitHub Actions.
 - [ ] Estrutura inicial de uma arquitetura, utilizando design system próprio, baseado no `React Spectrum`, da Adobe.
@@ -11,6 +13,7 @@ Esse projeto tem o objetivo de reunir lógicas de inicialização de projetos fr
 - [ ] Gerenciador de Estados performático, usando `Context API` e recursos do `React`.
 
 ## Motivação
+
 A criação desse projeto template tem como motivação inicial a otimização de tempo na criação de novos projetos, tornando o processo de desenvolvimento de software mais rápido e eficiente. Além disso, visa reduzir "dores de cabeça" na configuração de ferramentas, deixando o desenvolvedor apto a focar na implementação das regras de negócio, cumprir requisitos funcionais. Por fim, acredito que o desenvolvimento de software deve ser o mais eficaz possível, pois erros podem levar a aumentos desnecessários nos custos de desenvolvimento e correção!
 
 ## Como instalar o projeto e configurá-lo para seu ambiente de trabalho?
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,30 +1,35 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  eslintPluginUnicorn.configs.recommended,
-  {
-    plugins: { prettier: eslintPluginPrettier },
-  },
-  {
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'unicorn/prevent-abbreviations': 'off',
-    }
-  },
-  {
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.browser },
     },
-  },
-]);
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    eslintPluginUnicorn.configs.recommended,
+    {
+        plugins: { prettier: eslintPluginPrettier },
+    },
+    {
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'unicorn/prevent-abbreviations': 'off',
+        },
+    },
+    {
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+])
